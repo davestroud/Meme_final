e8d646623d14e2e38af7442f2df86500678f5f25
davestroud <david@greymatter-ag.com>
2015-10-23 09:30:30 -0500

    Initial Commit

---
+++ Meme_final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme_final
+//
+//  Created by greymatter-home on 10/23/15.
+//  Copyright © 2015 greymatter-home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
