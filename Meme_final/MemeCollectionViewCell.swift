82818b539eab03ced5cf5b4a9561a7cf8725c31d
davestroud <david@greymatter-ag.com>
2015-11-03 16:39:39 -0600

    add MemeCollectionViewCell

---
+++ Meme_final/MemeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCollectionViewCell.swift
+//  Meme_final
+//
+//  Created by greymatter-home on 11/3/15.
+//  Copyright © 2015 greymatter-home. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
